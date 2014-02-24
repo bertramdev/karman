5fb044cc003fc09fcebe9d3a340715fbdb9cb147
David Estes <davydotcom@gmail.com>
2014-02-23 19:35:57 -0500

    fixing url mappings

--- grails-app/conf/KarmanUrlMappings.groovy
+++ grails-app/conf/KarmanUrlMappings.groovy
@@ -3,7 +3,8 @@ import org.springframework.context.ApplicationContext
 class KarmanUrlMappings {
 
 	static mappings = { ApplicationContext context ->
-		def config = getGrailsApplication().config.grails.plugins.karman
+
+		def config = context.grailsApplication.config.grails.plugins.karman
 		def serveLocalStorage = config.containsKey('serveLocalStorage') ? config.serveLocalStorage : true
 		def path = config.serveLocalMapping ?: 'storage'
 		
