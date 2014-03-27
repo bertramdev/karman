295216ff1a41d0c5d958f26bc8b3f63c9a48b427
David Estes <davydotcom@gmail.com>
2014-03-27 09:27:14 -0400

    bumping karman core version

--- KarmanGrailsPlugin.groovy
+++ KarmanGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import com.bertramlabs.plugins.karman.StorageProvider
 import com.bertramlabs.plugins.karman.local.LocalStorageProvider
 
 class KarmanGrailsPlugin {
-    def version         = "0.4.0"
+    def version         = "0.5.0"
     def grailsVersion   = "2.0 > *"
     def title           = "Karman Plugin"
     def author          = "David Estes"
