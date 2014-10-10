d9252d724c671d97b710384bc835f86cf29bc5e1
David Estes <davydotcom@gmail.com>
2014-10-10 16:56:04 -0400

    added karman 0.5.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'com.bertramlabs.plugins:karman-core:0.5.3'
+        compile 'com.bertramlabs.plugins:karman-core:0.5.5'
     }
 
     plugins {
