a5e715cdd028c48297946ee9c1fe0db6ea78bae6
David Estes <davydotcom@gmail.com>
2015-11-24 09:59:42 -0500

    version dependency bump

--- KarmanGrailsPlugin.groovy
+++ KarmanGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import com.bertramlabs.plugins.karman.StorageProvider
 import com.bertramlabs.plugins.karman.local.LocalStorageProvider
 
 class KarmanGrailsPlugin {
-    def version         = "0.8.3"
+    def version         = "0.9.6"
     def grailsVersion   = "2.2 > *"
     def title           = "Karman Plugin"
     def author          = "David Estes"
