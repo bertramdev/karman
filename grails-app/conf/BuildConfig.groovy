72065e06ff7d362c1bce33b96e155779344c07d1
David Estes <davydotcom@gmail.com>
2016-01-22 12:48:22 -0500

    well this is new

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,7 +13,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'com.bertramlabs.plugins:karman-core:0.9.9'
+        compile 'com.bertramlabs.plugins:karman-core:0.9.11'
     }
 
     plugins {
