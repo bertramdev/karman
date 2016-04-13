185d22c45e9fedfaab70506926d2e5cdab1de01c
David Estes <davydotcom@gmail.com>
2016-04-13 12:11:08 -0400

    version bump

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,7 +13,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'com.bertramlabs.plugins:karman-core:0.9.19'
+        compile 'com.bertramlabs.plugins:karman-core:0.9.20'
     }
 
     plugins {
